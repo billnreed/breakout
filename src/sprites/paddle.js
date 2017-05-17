d4fd44f7fd088f77d880b3d7116e21ca1628641d
Bill Reed <billnreed@gmail.com>
2017-05-16 22:36:48 -0400

    use constant for paddle speed

--- src/sprites/paddle.js
+++ src/sprites/paddle.js
@@ -4,6 +4,8 @@ export default class extends Phaser.Sprite {
   constructor(game) {
     super(game, 0, 0, 'paddle');
 
+    this.paddleSpeed = 200;
+
     this.anchor.set(0.5, 0.5);
 
     game.physics.arcade.enable(this);
@@ -28,9 +30,9 @@ export default class extends Phaser.Sprite {
 
   update() {
     if (this.leftKey.isDown) {
-      this.body.velocity.x = -120;
+      this.body.velocity.x = -1 * this.paddleSpeed;
     } else if (this.rightKey.isDown) {
-      this.body.velocity.x = 120;
+      this.body.velocity.x = this.paddleSpeed;
     } else {
       this.body.velocity.x = 0;
     }
