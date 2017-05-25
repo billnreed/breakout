3b43dae32949b2f690ee81b75b366dae7fdc613e
Bill Reed <billnreed@gmail.com>
2017-05-24 20:29:03 -0400

    randomly choose which powerup is dropped

--- src/groups/powerups.js
+++ src/groups/powerups.js
@@ -6,15 +6,18 @@ import { WiderPaddlePowerup } from 'src/sprites/powerups/wider-paddle-powerup';
 export class Powerups extends Phaser.Group {
   constructor(game, spawnChance) {
     super(game, null, 'powerups', false, true, Phaser.Physics.ARCADE);
-    this.classType = WiderPaddlePowerup;
 
+    this.powerups = [ PaddleSpeedPowerup, WiderPaddlePowerup ];
     this.spawnChance = spawnChance;
   }
 
   spawnFromBrick(brick) {
     const { worldPosition: position } = brick;
     if (this.game.rnd.frac() < this.spawnChance) {
-      const powerup = this.create(position.x, position.y);
+      const PowerupClass = this.game.rnd.pick(this.powerups);
+
+      const powerup = new PowerupClass(this.game, position.x, position.y);
+      this.add(powerup);
       powerup.startMovement();
     }
   }
